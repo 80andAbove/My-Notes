05ba3aa2fb7e9775fef24bea1777dee9655b0a62
80andAbove <shogomiyagi94@gmail.com>
2021-09-15 00:53:37 +0900

    Created data.js in assets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Header from './components/Header';
-// import Body from './components/Body';
 import NotesListPage from './pages/NotesListPage';
 
 function App() {
---
+++ src/assets/data.js
@@ -0,0 +1,14 @@
+let notes = [
+    {
+        "id": 1,
+        "body": "Go for a walk",
+    },
+    {
+        "id": 2,
+        "body": "Take out the trash",
+    },
+    {
+        "id": 3,
+        "body": "Text Jess",
+    },
+]
