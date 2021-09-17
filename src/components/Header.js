82edd5603a89e2d7c124fce486456c8b4ff939b6
80andAbove <shogomiyagi94@gmail.com>
2021-09-18 02:16:05 +0900

    Added header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return(
-        <div className="header">
-            <h1>Notes</h1>
+        <div className="app-header">
+            <h1>Notes List</h1>
         </div>
     )
 }
