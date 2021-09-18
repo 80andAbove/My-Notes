55d7c5590dd165a42aeeec4549617436c1fa56a0
80andAbove <shogomiyagi94@gmail.com>
2021-09-18 20:19:26 +0900

    Implemented state

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import notes from '../assets/data'
+import React, { useState, useEffect } from 'react'
+// import notes from '../assets/data'
 import { ReactComponent as ArrowLeft } from '../assets/icons/chevron-left.svg'
 import { Link } from 'react-router-dom'
 
 const NotePage = ({match}) => {
     let noteId = match.params.id
 
-    let note = notes.find(note => note.id === Number(noteId)) 
+    // let note = notes.find(note => note.id === Number(noteId)) 
 
+    let [note, setNotes] = useState([null])
+
+    useEffect(() => {
+        getNotes()
+    }, [noteId])
+
+    let getNotes = async () => {
+        let response = await fetch(`http://localhost:8000/notes/${noteId}`)
+        let data = await response.json()
+        console.log('data:', data)
+        setNotes(data)
+    }
     return (
         <div className="note">
             <div className="note-header">
